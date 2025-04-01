8a32bdb3e393420428fac9a86d0aa4445b410062
gioannou <dev.gioannou@gmail.com>
2025-04-01 15:54:28 +0300

    Add type

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import AppBar from '$lib/components/AppBar.svelte';
+	import type { LayoutProps } from './$types';
+
 	import '../app.css';
 
-	let { children, data } = $props();
+	let { children, data }: LayoutProps = $props();
 </script>
 
 <div class="container mx-auto p-4">
