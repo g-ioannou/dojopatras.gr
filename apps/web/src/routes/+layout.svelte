02219d4df2f0c16fda0616b7d124f92bf7d94a54
gioannou <dev.gioannou@gmail.com>
2025-04-01 23:04:43 +0300

    Add css colors

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -9,25 +9,25 @@
 	let { children, data }: LayoutProps = $props();
 </script>
 
+<div class="fixed z-[-1] mx-auto bg-background flex justify-center w-screen">
+	<Masonry gridGap={0.8}>
+		{#if data.media?.docs}
+			{#each data.media.docs as media (media.id)}
+				<img
+					src={media.url}
+					alt={media.alt}
+					class="w-full rounded-[5px] brightness-50 sepia-[.6]"
+					loading="lazy"
+				/>
+			{/each}
+		{/if}
+	</Masonry>
+</div>
+
 <div class="flex min-h-screen flex-col">
-	<div class="fixed z-[-1] mx-auto self-center">
-		<Masonry gridGap={0.8}>
-			{#if data.media?.docs}
-				{#each data.media.docs as media (media.id)}
-					<img
-						src={media.url}
-						alt={media.alt}
-						class="w-full rounded-[5px] brightness-50 sepia-[.6]"
-						loading="lazy"
-					/>
-				{/each}
-			{/if}
-		</Masonry>
-	</div>
 	<AppBar deviceType={data.deviceType} />
-	<main class="grow bg-blue-300">
+	<main class="grow">
 		{@render children()}
-        main
 	</main>
-	<footer class="bg-yellow-300">footer</footer>
+	<footer class="min-h-56 text-foreground"> aaaaaa  footer</footer>
 </div>
