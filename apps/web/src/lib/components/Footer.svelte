03652611a22765274f1f49bee2a5c89f601ca966
gioannou <dev.gioannou@gmail.com>
2025-04-02 00:40:27 +0300

    Add some css classes to footer and header

--- apps/web/src/lib/components/Footer.svelte
+++ apps/web/src/lib/components/Footer.svelte
@@ -1,8 +1,10 @@
-<footer class="min-h-56 bg-foreground text-background drop-shadow-top">
-	<div class="container mx-auto flex">
-		<div class="text-center">
-			<img src="/logo_graphics_only.png" />
-            DOJO PATRAS
+<footer class="min-h-56 bg-foreground text-background drop-shadow-top p-4">
+	<div class="container mx-auto flex align-center">
+		<div class="flex flex-col justify-center items-center">
+			<div>
+				<img src="/logo_graphics_only.png" alt="Footer Dojo Patras logo" class="h-20" />
+			</div>
+			DOJO PATRAS
 		</div>
 	</div>
 </footer>
--- apps/web/src/lib/components/WebMenu.svelte
+++ apps/web/src/lib/components/WebMenu.svelte
@@ -1,1 +1,11 @@
-this is the web menu
+<div class="bg-background shadow-xl">
+	<div class="container py-4 md:py-8 flex justify-between">
+		<div>
+			<img src="/logo_with_name.png" class="h-10 md:h-14" />
+		</div>
+        <div>
+            hi
+        </div>
+
+	</div>
+</div>
