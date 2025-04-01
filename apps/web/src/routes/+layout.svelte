e550d818e96ec8557e91bc5b927fa35ead6056d5
gioannou <dev.gioannou@gmail.com>
2025-04-01 14:28:12 +0300

    Add shadcn

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+    import '../app.css'
+
+	let { children } = $props();
+</script>
+
+this is the layout
+<Button> test</Button>
+{@render children()}
--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -2,5 +2,10 @@ import { payload as P } from '$lib/server';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	return { users: await P?.find({ collection: 'users' }), media: await P?.find({collection:'media'}) };
+	return {
+		users: await P?.find({ collection: 'users' }),
+		media: await P?.find({ collection: 'media' }),
+        news: await P?.find({collection: 'news-and-announcements'})
+
+	};
 };
