5b0084450577df2505f5b25c83e1fd10a5b26f3d
gioannou <dev.gioannou@gmail.com>
2025-04-01 17:47:04 +0300

    Add flowbite

---
+++ apps/web/src/lib/components/NavBar.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/state';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	let activeUrl = page.url.pathname;
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<img src="/logo_with_name.png" class="me-3 h-10 sm:h-14" alt="Dojo Patras logo" />
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl}>
+		<NavLi href="/">Αρχική</NavLi>
+		<NavLi href="/classes">Τμήματα</NavLi>
+		<NavLi href="/academy">Η σχολή</NavLi>
+		<NavLi href="/contact">Επικοινωνία</NavLi>
+	</NavUl>
+</Navbar>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import NavBar from '$lib/components/NavBar.svelte';
+import { Button } from 'flowbite-svelte';
 	export let data;
 </script>
 
+<NavBar />
 <h1>Web</h1>
 <pre>{JSON.stringify(data, null, 4)}</pre>
+<Button>hi</Button>
 {#if data.media?.docs}
 	{#each data.media.docs as media (media.id)}
-		<img src="{media.url}" alt={media.alt} />
+		<img src={media.url} alt={media.alt} />
 	{/each}
 {/if}
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
