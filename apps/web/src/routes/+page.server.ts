ebe3b54876c7261a6a62f67bab25a09b6bbec17b
gioannou <dev.gioannou@gmail.com>
2025-04-01 21:57:41 +0300

    Add background masonry

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -3,8 +3,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {
-		users: await P?.find({ collection: 'users' }),
-		media: await P?.find({ collection: 'media', pagination: false }),
-        news: await P?.find({collection: 'news-and-announcements'})
+		media: await P?.find({ collection: 'media', pagination: false })
 	};
 };
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Masonry from '$lib/components/Masonry.svelte';
 
 	export let data;
-
 </script>
 
 <h1>Web</h1>
@@ -10,7 +9,11 @@
 <Masonry gridGap={0.8}>
 	{#if data.media?.docs}
 		{#each data.media.docs as media (media.id)}
-			<img src={media.url} alt={media.alt} class="w-full rounded-[5px]  brightness-50" />
+			<img
+				src={media.url}
+				alt={media.alt}
+				class="w-full rounded-[5px] brightness-50 sepia-[.6]"
+			/>
 		{/each}
 	{/if}
 </Masonry>
